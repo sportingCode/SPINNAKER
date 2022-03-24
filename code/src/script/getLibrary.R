b9830bd8f9a9b3c96c0b19e4e03bdba7f62a0ba4
sportingCode <82087443+sportingCode@users.noreply.github.com>
2022-03-24 13:26:40 +0100

    Add files via upload

--- code/src/script/getLibrary.R
+++ code/src/script/getLibrary.R
@@ -1,16 +1,37 @@
 getLibrary <- function(){
   
-  # if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-  # BiocManager::install("Biostrings")
+  ########################################
   
-  library(Biostrings)
-  library(data.table)
-  library(readxl)
-  library(R.utils)
-  library(plyr)
-  library(propagate)
-  library(stringi)
-  library(pheatmap)
+  print("Checking Dependency...")
+  
+  packages <- c("Biostrings","data.table","readxl","R.utils","plyr","propagate","stringi","pheatmap")
+  dependency <- data.frame(package = packages, repository = c("Bioc",rep("CRAN",7)), row.names = packages)
+  
+  installed <- installed.packages()
+  
+  if(!"devtools" %in% installed){install.packages('devtools')}
+  if(!"BiocManager" %in% installed){install.packages('BiocManager')}
+  
+  install <- dependency[!(dependency$package %in% installed),]
+  
+  if(nrow(install)>0){
+    
+    CRAN <- install$package[which(install$repository == 'CRAN')]
+    Bioc <- install$package[which(install$repository == 'Bioc')]
+    
+    if(length(CRAN)>0){devtools::install_cran(CRAN,upgrade = 'never')}
+    if(length(Bioc)>0){BiocManager::install(Bioc,update = F)}
+  }
+  
+  print("Checking Dependency Finished!")
+  ########################################
+  
+  print("Loading packages...")
+  
+  invisible(sapply(packages,require, character.only = TRUE))
+  
+  print("Loading packages Finished")
+
 }
 
 
